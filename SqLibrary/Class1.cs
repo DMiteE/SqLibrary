80aa4e89b352abe1e0bb2d9955f57811b985313c
DMiteE <praimebotob@gmail.com>
2022-01-07 09:50:55 +0300

    Добавьте файлы проекта.

---
+++ SqLibrary/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqLibrary
+{
+    public class Square
+    {
+        public static void triangle(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double S = Math.Round(Math.Sqrt((p * (p - a) * (p - b) * (p - c))), 2);;
+            MessageBox.Show("Площадь треугольника равна: " + S);
+
+        }
+        public static void circle(int r)
+        {
+            double pi = 3.14;
+            double S = pi * Math.Pow(r, 2);
+            MessageBox.Show("Площадь круга равна: " + S);
+        }
+
+
+    }
+}
